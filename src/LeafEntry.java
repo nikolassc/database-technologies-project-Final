d1f0ecde036b7d9d86c52f45388788fbc9e2c085
nikkliaf <nik.kliaf1@gmail.com>
2025-05-21 15:40:53 +0300

    fixed IndexToCSVExporter

--- src/LeafEntry.java
+++ src/LeafEntry.java
@@ -5,8 +5,16 @@ import java.util.ArrayList;
 // is the bounding box of the spatial object (the record) indexed
 // also holds the recordId of the record and a pointer of the block which the record is saved in the datafile
 public class LeafEntry extends Entry {
+    private long datafileBlockId;
     public LeafEntry(long datafileBlockId, MBR mbr) {
         super(mbr);  // sets bounding box
+        this.datafileBlockId = datafileBlockId;
         this.setChildNodeBlockId(datafileBlockId);  // sets block pointer
+    }
+
+
+
+    public long getDataBlockId() {
+        return datafileBlockId;
     }
 }
